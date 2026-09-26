4c44a5755269c7e6629a401e5168af496adcf760
wk8vtg9h7j-bit <wk8vtg9h7j@privaterelay.appleid.com>
2026-09-26 17:45:30 +0700

    Allow connected studio account to send booking emails

--- src/lib/google/oauth.ts
+++ src/lib/google/oauth.ts
@@ -18,6 +18,7 @@ const SCOPES = [
   "https://www.googleapis.com/auth/calendar.events",
   "https://www.googleapis.com/auth/calendar.readonly",
   "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/gmail.send",
 ];
 
 function required(name: string): string {
